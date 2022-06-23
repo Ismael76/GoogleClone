59f1ca5e7dfa0e67e31bee1769b198711a34e830
Ismael Ali <ismaelali78@outlook.com>
2022-06-23 16:57:11 +0100

    Working however one slight error

--- client/search.js
+++ client/search.js
@@ -7,6 +7,8 @@ let sectionAddTo = document.querySelector(".container");
 
 let search = localStorage.getItem("searchValue");
 
+let accept = localStorage.getItem("value");
+
 const getResult = async () => {
   const response = await fetch("http://localhost:5500/search");
   const data = await response.json();
@@ -20,5 +22,42 @@ const getResult = async () => {
     }
   }
 };
+
+const getAll = async (e) => {
+  const response = await fetch("http://localhost:5500/search");
+
+  const data = await response.json();
+
+  for (let i = 0; i < data.length; i++) {
+    let section = document.createElement("section");
+    let anchorTitle = document.createElement("a");
+    let paraURL = document.createElement("p");
+    let paraDesc = document.createElement("p");
+    let paraTags = document.createElement("p");
+
+    sectionAddTo.append(section);
+
+    paraURL.innerText = data[i].URL;
+    anchorTitle.innerText = data[i].Title;
+    anchorTitle.setAttribute("href", data[i].URL);
+    paraDesc.innerText = data[i].Description;
+    paraTags.innerText = data[i].Tags;
+
+    paraURL.classList.add("url");
+    anchorTitle.classList.add("title");
+    paraDesc.classList.add("desc");
+    paraTags.classList.add("tags");
+
+    section.append(anchorTitle);
+    section.append(paraURL);
+    section.append(paraDesc);
+    section.append(paraTags);
+  }
+};
+
+if (accept === "accept") {
+  getAll();
+  localStorage.setItem("value", "reset");
+}
 
 getResult();
--- client/session.js
+++ client/session.js
@@ -3,25 +3,6 @@ let searchInput = document.getElementById("searchInput");
 
 let allBtn = document.getElementById("all");
 
-let url = document.querySelector(".url");
-let title = document.querySelector(".title");
-let desc = document.querySelector(".desc");
-let tags = document.querySelector(".tags");
-
-const getResult = async () => {
-  const response = await fetch("http://localhost:5500/search");
-  const data = await response.json();
-  for (let i = 0; i < data.length; i++) {
-    if (search == data[i].Name) {
-      url.innerText = data[i].URL;
-      title.innerText = data[i].Title;
-      title.setAttribute("href", data[i].URL);
-      desc.innerText = data[i].Description;
-      tags.innerText = data[i].Tags;
-    }
-  }
-};
-
 const checkSearch = async (event) => {
   event.preventDefault();
 
@@ -32,7 +13,6 @@ const checkSearch = async (event) => {
     if (data[i].Name == searchInput.value.toLowerCase()) {
       localStorage.setItem("searchValue", searchInput.value.toLowerCase());
       window.location.href = event.target.href;
-      getResult();
       return;
     }
   }
@@ -47,8 +27,6 @@ const checkSearch = async (event) => {
 
 btn.addEventListener("click", checkSearch);
 
-allBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("clicked");
+allBtn.addEventListener("click", () => {
   localStorage.setItem("value", "accept");
 });
