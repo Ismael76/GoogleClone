4c93034e3d98c4931b774d6325986d5128d588e9
daiana.dobre@getfutureproo.co.uk <daiana_melania@yahoo.com>
2022-08-11 21:40:19 +0100

    test added for server

---
+++ server/tests/server.test.js
@@ -0,0 +1,32 @@
+const request = require ('supertest');
+const app = require("../server");
+const data = require("../data/data");
+
+describe ('API server', () => {
+    let api;
+
+    beforeAll( () => {
+        api = app.listen(3001, () => {
+            console.log("Test sever is running on 3001")
+        })
+    })
+
+    afterAll( (done) => {
+        console.log("Gracefully stoping test server");
+        api.close(done);
+    })
+
+    it('respondes to get / with a status of 200', (done) => {
+        request(api).get('/').expect(200, done);
+    })
+
+    it('respondes to get /google with a status of 200', (done) => {
+        request(api).get('/google').expect(200, done);
+    })
+
+    it('respondes to get /search with a status of 200 ', (done) => {
+        request(api).get('/search').expect(200, done);
+    })
+
+   
+})
